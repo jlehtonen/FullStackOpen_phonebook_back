20427c8b1812122236e24d9911d7f72b98af23c9
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-31 15:21:34 +0300

    Remove the unnecessary persons array

--- index.js
+++ index.js
@@ -21,29 +21,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :payload")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (req, res, next) => {
   Person.countDocuments({})
     .then(numPersons => {
@@ -71,10 +48,6 @@ app.post("/api/persons", (req, res, next) => {
 
   if (!person.number) {
     return res.status(400).json({ error: "number is required" });
-  }
-
-  if (persons.map(p => p.name).includes(person.name)) {
-    return res.status(400).json({ error: "name must be unique" });
   }
 
   const newPerson = new Person({
