882735bede38d7f163122fd8a38a66ded5d21e8d
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-31 13:40:21 +0300

    Complete exercise 3.15

--- index.js
+++ index.js
@@ -93,9 +93,9 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end();
+  });
 });
 
 const PORT = process.env.PORT;
